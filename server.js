aab50eaf4af8a66c875d71d3fba88fca0af8344f
Vidit Pandey <vidit.pandey@ad.infosys.com>
2019-12-14 22:00:59 +0100

    render html file via express server and not webpack-dev-server! Bundling needed via webpack.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const cors = require('cors')
 const crypto = require('crypto')
 const passport = require('passport')
@@ -21,59 +22,64 @@ const app = express()
 // set up basic configs
 app.set('view-engine', 'ejs')
 app.use(cors())
-app.use(express.urlencoded({ extended: false })) // to be able to use form elements (from /login or /register) in req obj
-app.use(flash())
-app.use(session({
-  secret: process.env.session_secret || 'to-do-login-secret',
-  resave: false,
-  saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(express.static(path.resolve(__dirname, './dist')))
 
-// view routes (to use sub apps concept later)
-app.get('/', checkAuthenticated, (req, res) => res.render('index.ejs', {name: req.user.name}))
+app.get('/api/list/:listId', (req, res, next) => { console.log(`reading for list ${req.params.listId}`); next() }, (req, res) => res.json({data: data(req.params.listId)}))
 
-app.route('/register')
-  .get(allowIfNotAuthenticated, (req, res) => res.render('register.ejs'))
-  .post(allowIfNotAuthenticated, (req, res) => {
-    const {name, email, password} = req.body
-    const hashedPassword = crypto.createHash('sha256').update(password).digest('hex')
-    users.push({
-      id: Date.now().toString(),
-      name,
-      email,
-      password: hashedPassword
-    })
-    console.log(users)
-    res.redirect('/login')
-  })
+app.get('*', function (req, res) {
+  res.sendFile(path.resolve(__dirname, './dist/index.html')).end()
+})
+// app.use(express.urlencoded({ extended: false })) // to be able to use form elements (from /login or /register) in req obj
+// app.use(flash())
+// app.use(session({
+//   secret: process.env.session_secret || 'to-do-login-secret',
+//   resave: false,
+//   saveUninitialized: false
+// }))
+// app.use(passport.initialize())
+// app.use(passport.session())
+// // view routes (to use sub apps concept later)
+// app.get('/', checkAuthenticated, (req, res) => res.render('index.ejs', {name: req.user.name}))
 
-app.route('/login')
-  .get(allowIfNotAuthenticated, (req, res) => res.render('login.ejs', {name: 'to-do app'}))
-  .post(allowIfNotAuthenticated, passport.authenticate('local', {
-    successRedirect: 'http://localhost:8080',
-    failureRedirect: '/login',
-    failureFlash: true
-  }))
+// app.route('/register')
+//   .get(allowIfNotAuthenticated, (req, res) => res.render('register.ejs'))
+//   .post(allowIfNotAuthenticated, (req, res) => {
+//     const {name, email, password} = req.body
+//     const hashedPassword = crypto.createHash('sha256').update(password).digest('hex')
+//     users.push({
+//       id: Date.now().toString(),
+//       name,
+//       email,
+//       password: hashedPassword
+//     })
+//     console.log(users)
+//     res.redirect('/login')
+//   })
 
-app.route('/logout')
-  .get((req, res) => {
-    req.logOut()
-    res.redirect('/login')
-  })
-function checkAuthenticated (req, res, next) {
-  console.log('TCL: checkAuthenticated -> req.isAuthenticated()', req.isAuthenticated())
-  if (req.isAuthenticated()) return next()
-  else res.redirect('/login')
-}
+// app.route('/login')
+//   .get(allowIfNotAuthenticated, (req, res) => res.render('login.ejs', {name: 'to-do app'}))
+//   .post(allowIfNotAuthenticated, passport.authenticate('local', {
+//     successRedirect: 'http://localhost:8080',
+//     failureRedirect: '/login',
+//     failureFlash: true
+//   }))
 
-function allowIfNotAuthenticated (req, res, next) {
-  if (req.isAuthenticated()) return res.redirect('/')
-  else next()
-}
+// app.route('/logout')
+//   .get((req, res) => {
+//     req.logOut()
+//     res.redirect('/login')
+//   })
+// function checkAuthenticated (req, res, next) {
+//   console.log('TCL: checkAuthenticated -> req.isAuthenticated()', req.isAuthenticated())
+//   if (req.isAuthenticated()) return next()
+//   else res.redirect('/login')
+// }
+
+// function allowIfNotAuthenticated (req, res, next) {
+//   if (req.isAuthenticated()) return res.redirect('/')
+//   else next()
+// }
 // API routes
-app.get('/api/list/:listId', (req, res) => res.json({data: data(req.params.listId)}))
 
 const port = 5001
 
