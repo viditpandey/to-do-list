d57bec77d6c499d395967d82551582ba5b011781
Vidit Pandey <vidit.pandey@metronom-external.com>
2020-11-18 23:36:13 +0530

    update backend URL to AWS deployed node instance

--- app/client/constants/constants.js
+++ app/client/constants/constants.js
@@ -1,1 +1,2 @@
-export const DEFAULT_BACKEND_URI = 'http://localhost:5001'
+// export const DEFAULT_BACKEND_URI = 'http://localhost:5001'
+export const DEFAULT_BACKEND_URI = 'http://nodetodolist-env.eba-z5yknppt.ap-south-1.elasticbeanstalk.com'
